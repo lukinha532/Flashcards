884ffa4d66cb17848992e44be75cb640289de03c
lukinha532 <lucas.marino.santos@escola.pr.gov.br>
2025-08-12 10:58:51 -0300

    js

---
+++ script.js
@@ -0,0 +1,17 @@
+
+Aviso: "// Seleciona todos os cartões const…"
+JOCEMARA DE FATIMA MARIANO
+Criado em: 5 de ago.5 de ago.
+// Seleciona todos os cartões
+const cartoes = document.querySelectorAll('.cartao');
+
+// Adiciona um evento de clique em cada cartão
+cartoes.forEach(cartao => {
+  cartao.addEventListener('click', () => {
+    // Seleciona o conteúdo interno do cartão
+    const conteudo = cartao.querySelector('.cartao__conteudo');
+   
+    // Alterna a classe 'girado' para aplicar a rotação
+    conteudo.classList.toggle('girado');
+  });
+})
